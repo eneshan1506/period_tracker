b2cb192c3876425fd2ecc0eaffe01678cb6c4267
Enes HAN <eneshan0646@gmail.com>
2026-05-07 22:57:27 +0200

    Add Android Capacitor build configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hanenes.cyclebloom',
+  appName: 'Cycle Bloom',
+  webDir: 'out'
+};
+
+export default config;
